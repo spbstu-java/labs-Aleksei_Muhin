c17e3425c64f4623e3f39db8a41dfd64cfbc8808
Aleksei Muhin <106451340+Leksssssssssssssss@users.noreply.github.com>
2023-11-08 21:37:16 +0300

    Delete lab 4 directory

--- lab 4/src/MyAnnotation.java
+++
@@ -1,7 +0,0 @@
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-public @interface MyAnnotation {
-    int value() default 1; // Целочисленный параметр аннотации, по умолчанию 1
-}
