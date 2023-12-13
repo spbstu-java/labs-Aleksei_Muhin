7de4db4a25eb19c9e95c30aff504325dfaca61d1
Нурмагомедов Хабиб <aleksei_muhin>
2023-12-13 19:21:38 +0300

    lab3

---
+++ lab3/src/Main.java
@@ -0,0 +1,9 @@
+import translator.Translator;
+
+public class Main {
+    public static void main(String[] args) {
+        Translator translator = new Translator();
+        translator.exec();
+    }
+}
+
